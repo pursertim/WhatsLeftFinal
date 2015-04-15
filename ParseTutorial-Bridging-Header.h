d02d7505a0508b6bf0db1ec59fdf45825dedbd9a
pursertim <timpurser@Tims-MacBook-Pro.local>
2015-04-15 22:56:29 +0100

    WhatsLeftFinalApplication

---
+++ ParseTutorial-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  ParseTutorial-Bridging-Header.h
+//  
+//
+//  Created by Tim Purser on 15/04/2015.
+//
+//
+
+#ifndef _ParseTutorial_Bridging_Header_h
+#define _ParseTutorial_Bridging_Header_h
+
+
+#endif
---
+++ ParseTutorial/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  ParseTutorial
+//
+//  Created by Tim Purser on 15/04/2015.
+//  Copyright (c) 2015 Tim Purser. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: PFTableViewCell {
+    
+    @IBOutlet weak var customNameEnglish: UILabel!
+    @IBOutlet weak var customCapital: UILabel!
+    @IBOutlet weak var customFlag: PFImageView!
+    
+}
